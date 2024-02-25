9573f3db04ee07732b5f8fc315da3356b04a5850
xyzjgmday <daysubhiz@gmail.com>
2024-02-25 15:15:43 +0700

    tugas-11

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,31 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
+    <div class="container">
         @foreach ($products as $idx => $product)
             @if ($idx == 0 || $idx % 4 == 0)
                 <div class="row mt-4">
             @endif
-            <div class="col-md-3">
-                <div class="card shadow">
-                    <img src="{{ route('products.image', ['imageName' => $product->image_url]) }}" alt="Product Image"
+            <div class="col">
+                <div class="card">
+                    <img src="{{ route('products.image', ['imageName' => $product->image_url]) }}" alt="image"
                         class="card-img-top">
                     <div class="card-body">
-                        <h5 class="card-title text-center">
-                            <a href="{{ route('products.show', ['id' => $product['id']]) }}" class="text-dark">{{ $product->name }}</a>
+                        <h5 class="card-title">
+                            <a href="{{ route('products.show', ['id' => $product['id']]) }}">{{ $product->name }}</a>
                         </h5>
-                        <p class="card-text text-center">
-                            Price: ${{ $product->price }}
+                        <p class="card-text">
+                            {{ $product->price }}
                         </p>
-                        <div class="text-center">
-                            <a href="{{ route('carts.add', ['id' => $product->id]) }}" class="btn btn-primary">Add to Cart</a>
-                        </div>
+                        <a href="{{ route('carts.add', ['id' => $product->id]) }}" class="btn btn-primary">Beli</a>
                     </div>
                 </div>
             </div>
-            @if ($idx > 0 && $idx % 4 == 3 || $idx == count($products) - 1)
-                </div>
-            @endif
-        @endforeach
+            @if ($idx > 0 && $idx % 4 == 3)
+    </div>
+    @endif
+    @endforeach
     </div>
 @endsection
